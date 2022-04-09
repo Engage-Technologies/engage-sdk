e78c9960b135bbdc7fd822016c996c1df6519be1
Luke Barbier <lukebarbier12@gmail.com>
2022-04-09 15:03:10 -0600

    new SDK utilizing tags & attributes

--- ServerScriptService/EngageBackendScript.lua
+++ ServerScriptService/EngageBackendScript.lua
@@ -1,8 +1,12 @@
 local ServerStorage = game:GetService("ServerStorage")
 local engageSDK = require(ServerStorage.EngageSDK.EngageSDKModule)
+local CollectionService = game:GetService("CollectionService")
 
+-- Zone Information
+local questionZoneInfo = {}
+
+-- Add joining of players
 local Players = game:GetService("Players")
-
 Players.PlayerAdded:Connect(
 	engageSDK.addPlayer
 )
@@ -10,3 +14,116 @@ Players.PlayerAdded:Connect(
 Players.PlayerRemoving:Connect(
 	engageSDK.removePlayer
 )
+
+local function findQuestionComponents(obj)
+	local components = {}
+	for _, child in ipairs(obj:GetDescendants()) do
+		if child:GetAttribute("EngageType") then
+			table.insert(components, child)
+		end
+	end
+	return components
+end
+
+-- Add Responses
+local numTags = script:GetAttribute("EngageNumTags")
+for i = 1, numTags do
+	-- Loop through all tags
+	local tagName = "QuestionZone" .. i
+	
+	local zoneObjects = CollectionService:GetTagged(tagName)
+	
+	local foundObjects = {
+		["response1"] = false,
+		["response2"] = false,
+		["response3"] = false
+	}
+	
+	for _, zoneObj in ipairs(zoneObjects) do
+		local components = findQuestionComponents(zoneObj)
+		
+		for _, component in ipairs(components) do
+			local engageType = component:GetAttribute("EngageType")
+			if engageType:match("response") then
+				foundObjects[engageType] = component
+			end
+		end
+	end
+	
+	for key, value in pairs(foundObjects) do
+		print(key .. " : " .. value.Name)
+	end
+	
+	-- Error check, make sure we have everything...
+	
+	-- Handle Question Zone
+	questionZoneInfo[tagName] = {
+		["question_instance_id"] = 1,
+		["response_type"] = "", -- assume we'll support more than multiple choice but plan for it initially
+		["option1"] = {}, -- mark one of these as the answer
+		["option2"] = {},
+		["option3"] = {}
+	}
+	
+	-- Handle responses
+	for key, responseObj in pairs(foundObjects) do
+		
+		local db = true -- does this variable get replicated OR need to be a global table?
+		
+		local option = responseObj:GetAttribute("EngageType")
+		
+		local function touched(touchedPart)
+			local partParent = touchedPart.Parent
+			-- Look for a humanoid in the parent
+			local humanoid = partParent:FindFirstChildWhichIsA("Humanoid")
+			if humanoid and db then
+				db = false
+				print("You selected " .. option)
+				
+				-- Check if correct
+				--if questionZoneInfo[tagName][option]["isAnswer"] then
+				--	print("You chose correctly!")
+				--else
+				--	print("Wrong answer!")
+				--end
+				
+				-- 
+				
+				-- we could send the http request from here! That would be simpler...
+				--module.leaveResponse(player_id, instace_id, response, correct, started_at, answered_at)
+				
+				
+				
+				wait(3)
+				db = true
+			end
+			
+		end
+		
+		responseObj.Touched:Connect(touched)
+	end
+end
+
+local function updateQuestionZone(zoneNum, playerId)
+	-- Update question Zone
+	
+	local tagName = "QuestionInstance" .. zoneNum
+	
+	-- Pull the new question
+	questionZoneInfo[tagName] = engageSDK.getQuestion(playerId)
+	
+	--questionZoneInfo[tagName] = {
+	--	["instance_id"] = 1,
+	--	["response_type"] = "", -- assume we'll support more than multiple choice but plan for it initially
+	--	["option1"] = {}, -- mark one of these as the answer
+	--	["option2"] = {},
+	--	["option3"] = {}
+	--}
+	for key, value in pairs(questionZoneInfo[tagName]) do
+		print(key .. " : " .. tostring(value))
+	end
+	
+	-- Loop through & update objects
+end
+
+updateQuestionZone(1, 3235295467)
---
+++ ServerStorage/EngageSDK/EnagageAPIWrapper.lua
@@ -0,0 +1,158 @@
+local module = {}
+
+local HttpService = game:GetService("HttpService")
+local ENGAGE_URL = "http://127.0.0.1:5001/gameplay/"
+
+local function parseBody(httpBody)
+	if httpBody ~= "" then
+		return HttpService:JSONDecode(httpBody) 
+	end
+	return nil
+end
+
+local function getRequest(uri_extension)
+	local response = HttpService:RequestAsync(
+		{
+			Url = ENGAGE_URL .. uri_extension,
+			Method = "GET"
+		}
+	)
+	return {response.StatusCode, parseBody(response.Body)}
+end
+
+local function postRequest(uri_extension, bodyDict)
+	local response = HttpService:RequestAsync(
+		{
+			Url = ENGAGE_URL .. uri_extension,
+			Method = "POST",
+			Headers = {
+				["Content-Type"] = "application/json"  -- When sending JSON, set this!
+			},
+			Body = HttpService:JSONEncode(bodyDict)
+		}
+	)
+	return {response.StatusCode, parseBody(response.Body)}
+end
+
+function module.addPlayer(player)
+
+	local url = "join/" .. game.GameId
+
+	local bodyDict = {
+		["player_id"] = player.UserId,
+		["player_name"] = player.Name
+	}
+
+	local resp
+
+	local function postRequestWrapper()
+		resp = postRequest(url, bodyDict)
+	end
+
+	local success, message = pcall(postRequestWrapper)
+
+	if success then
+		local status = resp[1]
+		if status == 204 then
+			return true
+		end
+	else
+		print(message)
+	end
+
+	return nil
+end
+
+function module.removePlayer(player)
+
+	local url = "leave/" .. game.GameId
+
+	local bodyDict = {
+		["player_id"] = player.UserId
+	}
+
+	local resp
+
+	local function postRequestWrapper()
+		resp = postRequest(url, bodyDict)
+	end
+
+	local success, message = pcall(postRequestWrapper)
+
+	if success then
+		local status = resp[1]
+		if status == 204 then
+			return true
+		end
+	else
+		print(message)
+	end
+
+	return nil
+end
+
+function module.getQuestion(player_id)
+
+	local url = "questions/" .. game.GameId .. "/" .. player_id
+
+	local resp
+
+	local function getRequestWrapper()
+		resp = getRequest(url)
+	end
+
+	local success, message = pcall(getRequestWrapper)
+
+	if success then
+		local status = resp[1]
+		local msg = resp[2]
+		if status == 200 then
+			return msg
+		end
+	else
+		print(message)
+	end
+
+	return nil
+
+end
+
+function module.leaveResponse(player_id, instace_id, response, correct, started_at, answered_at)
+
+	local url = "responses/" .. game.GameId .. "/" .. player_id
+
+	local bodyDict = {
+		["player_id"] = player_id,
+		["question_instance_id"] = instace_id,
+		["response"] = response,
+		["correct"] = correct
+	}
+	if started_at then
+		bodyDict["started_at"] = started_at
+	end
+	if answered_at then
+		bodyDict["answered_at"] = answered_at
+	end
+
+	local resp
+
+	local function postRequestWrapper()
+		resp = postRequest(url, bodyDict)
+	end
+
+	local success, message = pcall(postRequestWrapper)
+
+	if success then
+		local status = resp[1]
+		if status == 204 then
+			return true
+		end
+	else
+		print(message)
+	end
+
+	return nil
+
+end
+
+return module
